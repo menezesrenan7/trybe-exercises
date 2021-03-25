4d6e672843a22fd4946025392520bb4285732300
renan <renan_menezes07@hotmail.com>
2021-03-25 18:51:31 -0300

    Exercícios de 1 a 9

---
+++ bloco_4/dia_2/exercise_1.js
@@ -0,0 +1,5 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let indice = 0; indice < numbers.length; indice += 1) {
+    console.log(numbers[indice]);
+}
---
+++ bloco_4/dia_2/exercise_9.js
@@ -0,0 +1,9 @@
+let array = [];
+let divisao =[];
+
+for (index = 1; index <= 25; index += 1) {
+    array.push(index);
+    divisao.push(index / 2);
+}
+
+console.log(divisao);
