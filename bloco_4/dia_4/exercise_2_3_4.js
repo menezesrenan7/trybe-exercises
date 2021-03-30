51f56c6ed7e8711637c67b13f3682b985c53d491
renan <renan_menezes07@hotmail.com>
2021-03-30 11:40:18 -0300

    Exercício 1 sobre funções finalizado

--- bloco_4/dia_4/exercise_2_3_4.js
+++ bloco_4/dia_4/exercise_2_3_4.js
@@ -16,15 +16,3 @@ for (let key2 in info) {
   console.log(info[key2]);
 }
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim'
-}
-
-
-
-for (let key3 in info) {
-  console.log(info[key3] + ' e ' + info2[key3]); 
-}
