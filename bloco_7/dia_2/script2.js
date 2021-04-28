569dfc2f5b1955dab7c2f8224dcac72affa74ddc
renan <renan_menezes07@hotmail.com>
2021-04-28 09:03:05 -0300

    Realizado parte 2, exercício 6.

--- bloco_7/dia_2/script2.js
+++ bloco_7/dia_2/script2.js
@@ -18,26 +18,36 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Exercício 1.
-const addTurnoLesson2 = (obj, key, keyValue) => obj[key] = keyValue;
+// // Exercício 1.
+// const addTurnoLesson2 = (obj, key, keyValue) => obj[key] = keyValue;
 
-console.log(addTurnoLesson2(lesson2, 'turno', 'manhã'));
-console.table(lesson2);
+// console.log(addTurnoLesson2(lesson2, 'turno', 'manhã'));
+// console.table(lesson2);
 
-// Exercício 2.
-const keyList = obj => Object.keys(obj);
-console.log(keyList(lesson3));
+// // Exercício 2.
+// const keyList = obj => Object.keys(obj);
+// console.log(keyList(lesson3));
 
-// Exercício 3.
-const objectSize = obj => Object.keys(obj).length;
-console.log(objectSize(lesson3));
+// // Exercício 3.
+// const objectSize = obj => Object.keys(obj).length;
+// console.log(objectSize(lesson3));
 
-// Exercício 4.
-const objectValues = obj => Object.values(obj);
-console.log(objectValues(lesson3));
+// // Exercício 4.
+// const objectValues = obj => Object.values(obj);
+// console.log(objectValues(lesson3));
 
 // Exercício 5.
 const objectList = {lesson1, lesson2, lesson3};
 let allLessons = {};
 Object.assign(allLessons, objectList)
 console.log(allLessons);
+
+// Exercício 6.
+const totalStudents = obj => {
+  let sum = 0;
+  for (let lesson in obj) {
+    sum += Object.values(obj[lesson])[1];
+  }
+  return sum;
+}
+console.log(totalStudents(allLessons));
