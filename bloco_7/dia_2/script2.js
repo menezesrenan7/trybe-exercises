2a6698dae85c0d309356b2b41967889962453353
renan <renan_menezes07@hotmail.com>
2021-04-28 12:47:52 -0300

    Realizado os exercícios bônus.

--- bloco_7/dia_2/script2.js
+++ bloco_7/dia_2/script2.js
@@ -22,25 +22,20 @@ const lesson3 = {
 const addTurnoLesson2 = (obj, key, keyValue) => obj[key] = keyValue;
 
 console.log(addTurnoLesson2(lesson2, 'turno', 'manhã'));
-console.table(lesson2);
 
 // Exercício 2.
 const keyList = obj => Object.keys(obj);
-console.log(keyList(lesson3));
 
 // Exercício 3.
 const objectSize = obj => Object.keys(obj).length;
-console.log(objectSize(lesson3));
 
 // Exercício 4.
 const objectValues = obj => Object.values(obj);
-console.log(objectValues(lesson3));
 
 // Exercício 5.
 const objectList = {lesson1, lesson2, lesson3};
 let allLessons = {};
-Object.assign(allLessons, objectList)
-console.log(allLessons);
+Object.assign(allLessons, objectList);
 
 // Exercício 6.
 const totalStudents = obj => {
@@ -50,13 +45,38 @@ const totalStudents = obj => {
   }
   return sum;
 }
-console.log(totalStudents(allLessons));
 
 // Exercício 7.
 const getValueByNumber = (obj, position) => Object.values(obj)[position];
-console.log(getValueByNumber(lesson1, 0));
 
 // Exercício 8.
 const verifyPair = (obj, keyName, keyValue) => obj[keyName] === keyValue;
 
-console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Bônus 1.
+const countMathStudents = obj => {
+  let sum = 0;
+  for (let lesson in obj) {
+    if (Object.values(obj[lesson])[0] === 'Matemática') {
+      sum += Object.values(obj[lesson])[1];
+    }
+  }
+  return sum;
+}
+
+// Bônus 2.
+const createReport = (obj, professorName) => {
+  const report = new Object();
+  let aula = [];
+  let students = 0;
+  for (let lesson in obj) {
+    if (obj[lesson].professor === professorName) {
+      aula.push(obj[lesson].materia);
+      students += obj[lesson].numeroEstudantes;
+    }
+  }
+  report.professor = professorName;
+  report.aulas = aula;
+  report.estudantes = students;
+  return report;
+};
+console.log(createReport(allLessons, 'Carlos'));
