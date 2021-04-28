4ab00f96dbec11d695b880d10d3804b4a8fcf3b2
renan <renan_menezes07@hotmail.com>
2021-04-28 08:15:29 -0300

    Realizado parte 2, exercícios 1 e 2.

---
+++ bloco_7/dia_2/script2.js
@@ -0,0 +1,29 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Exercício 1.
+const addTurnoLesson2 = (obj, key, keyValue) => obj[key] = keyValue;
+
+console.log(addTurnoLesson2(lesson2, 'turno', 'manhã'));
+console.table(lesson2);
+
+// Exercício 2.
+const keyList = obj => Object.keys(obj);
+console.log(keyList(lesson3));
