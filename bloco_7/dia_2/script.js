903e1e9adf14e8cd97f4655a245ee6f8364b3416
renan <renan_menezes07@hotmail.com>
2021-04-28 07:57:38 -0300

    Realizado a parte 1, exercícios 1 e 2.

--- bloco_7/dia_2/script.js
+++ bloco_7/dia_2/script.js
@@ -34,16 +34,30 @@ const order = {
   },
 };
 
+
 const customerInfo = (order) => {
-  console.log(`Olá Ana Silveira, entrega para: ${order['name']}, Telefone: ${order['phoneNumber']}, ${Object.values(order['address'])}`);
+  console.log(`Olá ${Object.values(order.order.delivery)[0]}, entrega para: ${order['name']}, Telefone: ${order['phoneNumber']}, ${Object.values(order['address'])[0]}, Nº: ${Object.values(order['address'])[1]}, AP: ${Object.values(order['address'])[2]}`);
 
 }
 
 customerInfo(order);
 
 const orderModifier = (order) => {
-  // Adicione abaixo as informações necessárias.
-
+  order.name = 'Luiz Silva';
+  const mussarelaPizza = {
+    mussarela: {
+      amount: 1,
+      price: 25
+    }
+  };
+  const calabrezaPizza = {
+    calabreza: {
+      amount: 1,
+      price: 25
+    }
+  };
+  Object.assign(order.order.pizza, mussarelaPizza, calabrezaPizza);
+  console.log(`Olá, ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)[2]}, ${Object.keys(order.order.pizza)[3]} e  é R$ ${(order.order.pizza.mussarela.price) + order.order.pizza.calabreza.price}`);
 }
 
 orderModifier(order);
