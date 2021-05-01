f727f0c36535fbdf14f5d53a56e02e106bf790b2
renan <renan_menezes07@hotmail.com>
2021-05-01 18:13:46 -0300

    Resolução parcial do exercício 3 do bônus.

--- bloco_8/dia_1/bonus.js
+++ bloco_8/dia_1/bonus.js
@@ -34,4 +34,9 @@ const warriorDamage = () => {
 
 // 3. Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
-// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de 
+// dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+const mageDagame = () => ({
+  dano: () => (mage.intelligence * 2) - (Math.random() * mage.intelligence),
+})
+
