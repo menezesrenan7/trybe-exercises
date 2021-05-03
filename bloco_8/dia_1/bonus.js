d660c52e7e9c11dd432660c07e2f502538675916
renan <renan_menezes07@hotmail.com>
2021-05-03 12:13:08 -0300

    Resolução do bônus.

--- bloco_8/dia_1/bonus.js
+++ bloco_8/dia_1/bonus.js
@@ -23,20 +23,48 @@ const battleMembers = { mage, warrior, dragon };
 // 1. Crie uma função que retorna o dano do dragão.
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
 const dragonDamage = () => {
-  return 50 - Math.floor(Math.random() * 15);
+  return dragon.strength - Math.floor(Math.random() * 15);
 }
 
 // 2. Crie uma função que retorna o dano causado pelo warrior .
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
 const warriorDamage = () => {
-  return warrior.strength * (warrior.weaponDmg - (1 - Math.random() * warrior.weaponDmg));
+  return warrior.strength * (warrior.weaponDmg - Math.abs(Math.random() * warrior.weaponDmg - 1));
 }
 
 // 3. Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
 // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
 // A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de 
 // dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
-const mageDagame = () => ({
-  dano: () => (mage.intelligence * 2) - (Math.random() * mage.intelligence),
+const mageDamage = () => ({
+  dano: () => (mage.intelligence * 2) - Math.ceil(Math.random() * mage.intelligence),
+  mana: () => { 
+    if(mage.mana < 15) {
+      mageDamage.dano = 'Não possui mana suficiente.'
+    };
+      mage.mana -= 15;
+  }
 })
 
+const gameActions = ({
+  warriorTurn: (warriorDamage) => {
+    dragon.healthPoints -= warriorDamage();
+    warrior.damage += warriorDamage();
+  },
+  
+  mageTurn: (mageDamage) => {
+    dragon.healthPoints -= mageDamage();
+    mage.damage += mageDamage();
+    mage.mana -= 15;
+  },
+
+  dragonTurn: (dragonDamage) => {
+    warrior.healthPoints -= dragonDamage();
+    mage.healthPoints -= dragonDamage();
+    dragon.damage += dragonDamage();
+  },
+  
+  turnResult: () => battleMembers,
+})
+
+console.log(gameActions);
